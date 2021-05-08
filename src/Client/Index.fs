dd9e15d4de45e16791cfb4ee2a5f2b3e14ddef67
tot005 <tot005@post.uit.no>
2021-05-08 13:40:55 +0200

    Donegit add -A

--- src/Client/Index.fs
+++ src/Client/Index.fs
@@ -1,115 +1,418 @@
 module Index
 
 open Elmish
+
+open Shared
+open Feliz
+open Feliz.Bulma
 open Thoth.Json
 open Thoth.Fetch
-open Shared
+open System
 
-type Model = { Todos: Todo list; Input: string }
 
-type Msg =
-    | GotTodos of Todo list
+
+
+type Model = { 
+    Input : string
+    Rows : int list
+    Collums : char list
+    Cells : Sheet
+    SumInput : string
+    Sum : int
+    MultiInput : string
+    Multi : int
+    NewTodo : Todo option
+}
+
+type Msg = 
+    // | CsvSave
     | SetInput of string
-    | AddTodo
-    | AddedTodo of Todo
+    | SetSumInput of string
+    | SetMultiInput of string
+    | Exn of System.Exception
+    | DisplayCsv
+    | HandleReset
+    | CalculateSum
+    | CalculateMulti
+    | Init of Result<Todo,FetchError>
+    | Save
+    | SaveHandler of Result<Todo, FetchError>
+    | ResetHandler of Result<Todo,FetchError>
 
-let getTodos () =
-    let decoder : Decoder<Todo list> = Thoth.Json.Decode.Auto.generateDecoder ()
-    Fetch.fetchAs (url="/api/getTodos", decoder = decoder)
 
-let addTodo (todo: Todo) =
-    let decoder : Decoder<Todo> = Thoth.Json.Decode.Auto.generateDecoder ()
-    Fetch.post (url="/api/addTodo", data = todo, decoder = decoder)
+// let getTodos () =
+//     let decoder : Decoder<Todo list> = Thoth.Json.Decode.Auto.generateDecoder ()
+//     Fetch.fetchAs (url="/api/getTodos", decoder = decoder)
+
+// let addTodo (todo: Todo) =
+//     let decoder : Decoder<Todo> = Thoth.Json.Decode.Auto.generateDecoder ()
+//     Fetch.post (url="/api/addTodo", data = todo, decoder = decoder)
+
+let saveCsv model =
+    // let decoder : Decoder<string> = Thoth.Json.Decode.Auto.generateDecoder ()
+    let decoder = Decode.Auto.generateDecoder<Todo> ()
+    match model.NewTodo with
+    | Some csv ->
+        let p () = 
+            promise {
+                return! Fetch.tryPost (
+                    "/api/model",
+                    data = csv,
+                    decoder = decoder
+                )
+            }
+        model, Cmd.OfPromise.either p () SaveHandler Exn
+    | None ->
+            printfn " Save CSV not reached"
+            model, Cmd.none
 
 let init () : Model * Cmd<Msg> =
-    let model = { Todos = []; Input = "" }
-    let cmd =
-        Cmd.OfPromise.perform getTodos () GotTodos
-    model, cmd
+    let decoder = Decode.Auto.generateDecoder<Todo> ()
+    let p () = 
+        promise {
+            return! Fetch.tryGet("/api/model", decoder = decoder) 
+        }
+    let model = { 
+        Input = "" 
+        Rows = [1..15]
+        Collums = ['a'..'l']
+        Cells = Map.empty
+        SumInput = ""
+        Sum = 0
+        MultiInput = ""
+        Multi = 0
+        NewTodo = None
+    }
+    model, Cmd.OfPromise.either p () Init Exn
+
+   
+
+        
+let splitRows =
+    (fun (description : string) -> Seq.toList (description.Split "\n"))    
+
+let splitLines =
+    (fun (description : string) -> Seq.toList (description.Split ","))  
+
+let translateCsv input model = 
+    let rows = splitRows input
+    let mutable temp = 1
+    let mutable temp2 = 0
+
+    let mutable map = Map.empty<Position, string>
+
+    for row in rows do
+        let line = splitLines row
+        temp2 <- 0
+        for number in line do
+            if Todo.isValid number then
+                map <- Map.add (model.Collums.Item(temp2),temp) number map
+                temp2 <- temp2 + 1
+        temp <- temp + 1
+    map
+    
+let matchFind posC posR model =
+    let opt = Map.tryFind (posC, posR) model.Cells
+    match opt with
+    | Some x -> x 
+    | _ -> ""
+
+
+
+let handleReset model = 
+    let decoder = Decode.Auto.generateDecoder<Todo> ()
+    let p () =
+        promise { 
+            return! Fetch.tryDelete(
+                "/api/reset", 
+                data = "",
+                decoder = decoder
+            ) 
+        }
+    
+    let model = { 
+        Input = "" 
+        Rows = [1..15]
+        Collums = ['a'..'l']
+        Cells = Map.empty
+        SumInput = ""
+        Sum = 0
+        MultiInput = ""
+        Multi = 0
+        NewTodo = None
+        }    
+    model, Cmd.OfPromise.either p () ResetHandler Exn
+
+let sumRow model = 
+    let mutable sum = 0
+    let inputInt = model.SumInput |> int
+    for letter in model.Collums do
+        let cell = Map.tryFind (letter, inputInt) model.Cells
+        match cell with
+        | Some x -> sum <- sum + (x |> int)
+        | _ -> printfn "no value"
+    { model with Sum = sum }, Cmd.none
+
+   
+let sumCollum model = 
+    let mutable sum = 0
+    // let inputInt = model.SumInput |> int
+    let charInput = model.SumInput |> char
+    for numbers in model.Rows do
+        let cell = Map.tryFind (charInput, numbers) model.Cells
+        match cell with
+        | Some x -> sum <- sum + (x |> int)
+        | _ -> printfn "no value"
+    { model with Sum = sum }, Cmd.none
+        
+let calcSum model =
+    let inp = model.SumInput
+    match System.Int32.TryParse inp with
+    | true, num -> sumRow model
+    | _, num -> sumCollum model
+
+
+let multiRow model = 
+    let mutable multi = 1
+    let inputInt = model.MultiInput |> int
+    for letter in model.Collums do
+        let cell = Map.tryFind (letter, inputInt) model.Cells
+        match cell with
+        | Some x -> multi <- multi * (x |> int)
+        | _ -> printfn "no value!!!!!!"
+    { model with Multi = multi }, Cmd.none
+
+let multiCollum model = 
+    let mutable multi = 1
+    let charInput = model.MultiInput |> char
+    printfn "multiInput %A" model.MultiInput
+    for numbers in model.Rows do
+        let cell = Map.tryFind (charInput, numbers) model.Cells
+        match cell with
+        | Some x -> 
+        multi <- multi * (x |> int)
+        | _ -> printfn "no value"
+    { model with Multi = multi }, Cmd.none
+
+let calcMulti model =
+    let inp = model.MultiInput
+    match System.Int32.TryParse inp with
+    | true, num -> multiRow model
+    | _, num -> multiCollum model
+
+let handleInit model =
+    function
+    | Ok s -> 
+        let newCsv = translateCsv s.Description model
+        { model with 
+            Cells = newCsv 
+        } , Cmd.none
+    | Error err -> 
+        printfn "Error in handle init %A" err 
+        model, Cmd.none
+
+
+let UpdateModelInput model (update : Todo -> Todo) = 
+    let p = Option.defaultValue Todo.New model.NewTodo
+    let p' = update p
+    let model' = { model with 
+                    NewTodo = Some p' 
+                    Input = p'.Description 
+                }
+    model', Cmd.none 
+
+let addCsv model =
+    function
+    | Ok p ->
+        let map = translateCsv p.Description model
+        { model with 
+            Cells = map
+            NewTodo = None
+
+        }, Cmd.none
+    | Error err -> 
+        printfn "ERROR: addCsv (): %A" err
+        model,Cmd.none
+
+
+let reset (model : Model) =
+    function
+    | Ok s ->
+        model, Cmd.none
+    | Error e ->
+        printfn "Error in reset %A " e
+        model, Cmd.none 
+
 
 let update (msg: Msg) (model: Model) : Model * Cmd<Msg> =
     match msg with
-    | GotTodos todos -> { model with Todos = todos }, Cmd.none
-    | SetInput value -> { model with Input = value }, Cmd.none
-    | AddTodo ->
-        let todo = Todo.create model.Input
-        let cmd = Cmd.OfPromise.perform addTodo todo AddedTodo
-        { model with Input = "" }, cmd
-    | AddedTodo todo ->
-        { model with
-            Todos = model.Todos @ [ todo ]
-        },
-        Cmd.none
+    | SetInput value -> UpdateModelInput model (fun p -> { p with Description = value })
+    | SetSumInput value -> { model with SumInput = value }, Cmd.none 
+    | SetMultiInput value -> { model with MultiInput = value }, Cmd.none 
+    | DisplayCsv -> 
+        let test = translateCsv model.Input model
+        { model with 
+            Cells = test 
+            Input = ""}, Cmd.none
+    | HandleReset -> handleReset model
+    | CalculateSum -> calcSum model
+    | CalculateMulti -> calcMulti model
+    | Save -> saveCsv model
+    | SaveHandler x -> addCsv model x
+    | Init x -> handleInit model x
+    | Exn e -> printfn "Exn %A " e ; model, Cmd.none
+    | ResetHandler e -> reset model e
 
-open Feliz
-open Feliz.Bulma
+    
+let containerBox model dispatch =        
+    Bulma.box [
+        Bulma.table [            
+            Html.tr [
+                Html.th [
 
-let navBrand =
-    Bulma.navbarBrand.div [
-        Bulma.navbarItem.a [
-            prop.href "https://safe-stack.github.io/"
-            navbarItem.isActive
-            prop.children [ Html.img [
-                prop.src "/favicon.png"
-                prop.alt "Logo"
+                ]
+                for collums in model.Collums do
+                    Html.th [
+                        prop.style [
+                                style.fontSize 20
+                                style.borderColor "black"
+                                style.border(2, borderStyle.solid, color.black)
+                        ]
+
+                        
+                        prop.text(string collums)
+                    ]
+
+            ]
+            for rows in model.Rows do
+                Html.tr [
+                    Html.th [
+                        prop.style [
+                            style.fontSize 20
+                            style.borderColor "black"
+                            style.border(2, borderStyle.solid, color.black)
+                        ]
+                        prop.text(rows)
+                    ]
+                    for collums in model.Collums do
+                    Html.th [
+                        prop.style [
+                            style.fontSize 20
+                            style.borderColor "black"
+                            style.border(2, borderStyle.solid, color.black)
+                        ]
+                        // Map.tryFind pos model.Cells
+                        prop.text (matchFind collums rows model)
+
+                    ]
+                ]
+        ]
+        Bulma.button.a [
+            color.isDanger
+            prop.text "Reset"
+            prop.onClick (fun _ -> dispatch HandleReset)
+        ]
+
+        Bulma.box [
+            Html.p [
+                prop.text "To sum or multiply a row input the number or letter that represents the row"
+            ] 
+            Html.input [
+                prop.style [ 
+                    style.width 150
+                    style.marginTop 10
+                    style.marginLeft 10
+                    style.marginRight 20
+                    ]
+                //Må huske å sjekke etter valud input
+                prop.value model.SumInput
+                prop.placeholder ""
+                prop.onChange (fun x -> SetSumInput x |> dispatch)
+
+            ]
+            Bulma.button.a [
+                color.isDark
+                prop.text "Sum"
+                prop.onClick (fun _ -> dispatch CalculateSum)
+            ]
+            Html.input [
+                prop.style [ 
+                    style.width 100
+                    style.marginTop 10
+                    style.marginLeft 10
+                    style.marginRight 20
+                    // style.backgroundColor "red"
+                    ]
+                prop.value model.Sum
+                prop.placeholder "Sum"
+
+            ]
+        ]
+
+        // MULTI
+        Bulma.box [
+            Html.input [
+                prop.style [ 
+                    style.width 150
+                    style.marginTop 10
+                    style.marginLeft 10
+                    style.marginRight 20
+                    ]
+                //Må huske å sjekke etter valud input
+                prop.value model.MultiInput
+                prop.placeholder ""
+                prop.onChange (fun x -> SetMultiInput x |> dispatch)
+
+            ]
+            Bulma.button.a [
+                color.isDark
+                prop.text "Multiply"
+                prop.onClick (fun _ -> dispatch CalculateMulti)
+            ]
+            Html.input [
+                prop.style [ 
+                    style.width 100
+                    style.marginTop 10
+                    style.marginLeft 10
+                    style.marginRight 20
+                    // style.backgroundColor "green"
+                    ]
+                prop.value model.Multi
+                prop.placeholder ""
+
+            ]
+        ]
+
+
+
+
+        Bulma.control.p [
+            control.isExpanded
+
+            prop.children [ 
+               Bulma.textarea [
+               prop.value model.Input
+               prop.placeholder "Input Csv"
+               prop.onChange (fun x -> SetInput x |> dispatch)
             ] ]
         ]
+        Bulma.control.p [ Bulma.button.a [
+            color.isPrimary
+            prop.disabled ( Todo.isValid model.Input |> not)
+            prop.onClick (fun _ -> dispatch Save )
+            prop.text "Save"
+        ] ]
     ]
-
-let containerBox (model: Model) (dispatch: Msg -> unit) =
-    Bulma.box [
-        Bulma.content [
-            Html.ol [
-                for todo in model.Todos do
-                    Html.li [ prop.text todo.Description ]
-            ]
-        ]
-        Bulma.field.div [
-            field.isGrouped
-            prop.children [
-                Bulma.control.p [
-                    control.isExpanded
-                    prop.children [ Bulma.input.text [
-                       prop.value model.Input
-                       prop.placeholder "What needs to be done?"
-                       prop.onChange (fun x -> SetInput x |> dispatch)
-                    ] ]
-                ]
-                Bulma.control.p [ Bulma.button.a [
-                    color.isPrimary
-                    prop.disabled ( Todo.isValid model.Input |> not)
-                    prop.onClick (fun _ -> dispatch AddTodo)
-                    prop.text "Add"
-                ] ]
-            ]
-        ]
-   ]
+       
 
 let view (model: Model) (dispatch: Msg -> unit) =
-    Bulma.hero [
-        hero.isFullHeight
-        color.isPrimary
-        prop.style [
-            style.backgroundSize "cover"
-            style.backgroundImageUrl "https://unsplash.it/1200/900?random"
-            style.backgroundPosition "no-repeat center center fixed"
-        ]
+    Bulma.container [
         prop.children [
-            Bulma.heroHead [
-                Bulma.navbar [ Bulma.container [ navBrand ] ]
-            ]
-            Bulma.heroBody [ Bulma.container [
-                Bulma.column [
-                    column.is6
-                    column.isOffset3
-                    prop.children [
-                        Bulma.title [
-                            text.hasTextCentered
-                            prop.text "foo"
-                        ]
-                        containerBox model dispatch
-                    ]
-                ] ]
-            ]
+            Bulma.title [
+                // text.hasTextCentered
+                prop.text "Excell"
+            ] 
+            containerBox model dispatch
         ]
     ]
+    
--- src/Shared/Shared.fs
+++ src/Shared/Shared.fs
@@ -2,14 +2,46 @@ namespace Shared
 
 open System
 
-type Todo = { Id: Guid; Description: string }
+
+
+
+type Position = char * int
+
+type Sheet = Map<Position, string>
+
+
+
+
+type Todo = {  
+    Description: string 
+    
+} with 
+    static member New = {
+        Description = ""
+    }
 
 module Todo =
-    let isValid (description: string) =
-        String.IsNullOrWhiteSpace description |> not
 
-    let create (description: string) =
-        {
-            Id = Guid.NewGuid()
-            Description = description
-        }
+    let (|Integer|_|) (str: string) =
+        let mutable intvalue = 0
+        if System.Int32.TryParse(str, &intvalue) then Some(intvalue)
+        else None
+
+    let isValidNumber (desc : string) = 
+        match desc with
+        // | Float desc -> true
+        | Integer desc -> true
+        | "\n" -> true
+        | _ -> false
+
+    let findNumbers =
+        (fun (description : string) -> Seq.toList (description.Split ','))
+
+    let isValid (description: string) : bool =
+
+        let stringlist = findNumbers description
+
+        match stringlist |> List.tryFind (fun x -> isValidNumber x = false) with
+        | Some _ -> false
+        | None -> true
+        
